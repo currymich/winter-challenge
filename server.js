e002b4cd997dfd54b3cc99d0879435d981469309
currymich <currymich@gmail.com>
2017-08-10 16:20:37 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send({ hi: "there" });
+});
+
+app.listen(5000, () => {
+  console.log('Server listening on 5000');
+})
