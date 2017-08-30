c9eb489803471a152c553da66d60512a036c4132
currymich <currymich@gmail.com>
2017-08-30 15:54:00 -0700

    fixing typos on keys file

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === "production") {
 	// we are in production, return prod keys
-	module.exports = requre("./prod");
+	module.exports = require("./prod");
 } else {
 	// we are in development, return dev keys
 	module.exports = require("./dev");
