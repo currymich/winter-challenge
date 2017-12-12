a0d4f349c474fd1d18b521c3a50649a9a02d6d18
currymich <currymich@gmail.com>
2017-12-12 13:37:07 -0800

    'connect' file to redux, use auth state to determine which button shown

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderLogin() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login</a></li>;
+      default:  //user IS logged in
+        return <li><a href="/api/logout">Logout</a></li>
+    }
+  }
+
   render() {
+    console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Element</a>
           <ul className="right">
-            <li><a href="/auth/google">Login</a></li>
+            {this.renderLogin()}
           </ul>
         </div>
       </nav>
@@ -15,4 +28,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps(state){
+  return { auth: state.auth };
+};
+
+export default connect(mapStateToProps)(Header);
