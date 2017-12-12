b293a3df84902988273cd158fba0daaaa41e3c73
currymich <currymich@gmail.com>
2017-12-11 18:43:17 -0800

    replacing default div with enabled routes connected to base components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,10 +5,20 @@ import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
+
 const App = () => {
   return (
-    <div>
-      Hi There!
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/surveys/new" component={SurveyNew} />
+            <Route path="/surveys" component={Dashboard} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
