6f5f34d3689c6dbc9d746757cb4cba9741515fbd
currymich <currymich@gmail.com>
2017-12-12 01:14:04 -0800

    Convert app to class based and hook up to redux with connect

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Switch>
-            <Route path="/surveys/new" component={SurveyNew} />
-            <Route path="/surveys" component={Dashboard} />
-            <Route path="/" component={Landing} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+    this.props.fetchUser();
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Switch>
+              <Route path="/surveys/new" component={SurveyNew} />
+              <Route path="/surveys" component={Dashboard} />
+              <Route path="/" component={Landing} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  };
 };
 
-export default App;
+export default connect(null, actions)(App);
