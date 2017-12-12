af47cea5653307177e28b481556bc4577ef2f7af
currymich <currymich@gmail.com>
2017-12-12 01:29:42 -0800

    refactor to async await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 export const fetchUser = () => {
-  return function(dispatch) {
-    axios.get('/api/current_user')
-    .then(res => dispatch({type: FETCH_USER, payload: res}));
+  return async dispatch => {
+    const res = await axios.get('/api/current_user'); dispatch({type: FETCH_USER, payload: res});
   }
 };
