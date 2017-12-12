a437c34754aa7cc72dc5f09d885aa36ebe232b38
currymich <currymich@gmail.com>
2017-12-11 18:40:38 -0800

    Wiring up default components to test routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 const App = () => {
   return (
     <div>
