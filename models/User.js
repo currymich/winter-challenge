7c4d662fcd1d8275a69ce7f5c99884564e9dd045
currymich <currymich@gmail.com>
2017-12-12 17:30:20 -0800

    fixing erraneous ;

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
 	googleId: String,
   name: String,
-	points: { type: Number, default: 0 };
+	points: { type: Number, default: 0 }
 });
 
 mongoose.model("users", userSchema);
