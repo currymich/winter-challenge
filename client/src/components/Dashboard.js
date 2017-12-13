8662e38f16183fc4028571aedb2edc0b39d85cc3
currymich <currymich@gmail.com>
2017-12-13 12:21:27 -0800

    New dashboard component

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class Dashboard extends Component {
+
+  renderWelcome(){
+    const {auth} = this.props;
+    return(auth ? `Welcome back ${auth.name}` : `User dashboard`);
+  }
+
+  render(){
+    return (
+      <div>
+        <div style={{ textAlign: 'center' }}>
+          <h3>{this.renderWelcome()}</h3>
+          <strong>Add completed goals below</strong>
+        </div>
+
+        <div style={{ marginTop: '10em' }}>
+          <div>Bible Reading entry form</div>
+          <div>Memory Verse entry form</div>
+          <div>Exercise entry form</div>
+          <div>Book Reading entry form</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state){
+  return { auth: state.auth };
+};
+
+export default connect(mapStateToProps)(Dashboard);
