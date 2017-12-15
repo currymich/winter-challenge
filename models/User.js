efca3b68781e44d22e9ab95db91968a5a56ac53a
currymich <currymich@gmail.com>
2017-12-15 00:06:39 -0800

    Adding goal to user model and wiring as reference

--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const goalSchema = require("./Goal")
+
 
 const userSchema = new Schema({
 	googleId: String,
   name: String,
-	points: { type: Number, default: 0 }
+	points: { type: Number, default: 0 },
+	goals: [{ type: Schema.Types.ObjectId, ref: 'Goal'}]
 });
 
 mongoose.model("users", userSchema);
