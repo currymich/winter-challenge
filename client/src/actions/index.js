3d2c1482f1f0ef7a706721bd686ce48a24181191
currymich <currymich@gmail.com>
2017-12-14 23:58:53 -0800

    Adding new FETCH_MEMORIZED action to pull all memorized verses from api

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_MEMORIZED } from './types';
 
 export const fetchUser = () => {
   return async dispatch => {
-    const res = await axios.get('/api/current_user'); dispatch({type: FETCH_USER, payload: res});
+    const res = await axios.get('/api/current_user');
+    dispatch({type: FETCH_USER, payload: res});
   }
 };
+
+export const fetchMemorizedVerses = () => {
+  return async dispatch => {
+    const res = await axios.get('/goals/bible_memory');
+    dispatch({type: FETCH_MEMORIZED, payload: res});
+  }
+}
