b15e0859bf10647c195ecdf222b138efed9049a3
currymich <currymich@gmail.com>
2017-12-15 00:08:59 -0800

    Added get route for bible reading and memorizing

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -3,4 +3,16 @@ const Goal = mongoose.model("goals");
 const User = mongoose.model("users");
 
 module.exports = app => {
+	app.get("/goals/bible_memory", async (req, res) => {
+		const memorized = await Goal.find({type: "bibleMemory"});
+
+		res.send(memorized);
+	});
+
+  app.get("/goals/bible_reading", async (req, res) => {
+    const read = await Goal.find({type: "bibleReading"});
+
+    res.send(read);
+  });
+
 };
