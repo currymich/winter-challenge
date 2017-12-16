89580333db3363ed12d04b8f71fa678da610575c
currymich <currymich@gmail.com>
2017-12-15 22:26:13 -0800

    point route takes points off each goal for logged in user and sum

---
+++ client/src/reducers/pointsReducer.js
@@ -0,0 +1,12 @@
+import { UPDATE_USER_POINTS } from '../actions/types';
+
+export default function(state = 0, action) {
+  switch (action.type) {
+    case(UPDATE_USER_POINTS):
+      const { data } = action.payload;
+      const points = data.reduce((sum, goal) => {return sum + goal.points}, 0);
+      return points;
+    default:
+      return state;
+  }
+}
