2fbda7f1c3ef0e87cce5c57800d95b28c2a2d7f2
currymich <currymich@gmail.com>
2017-12-15 17:36:19 -0800

    fixed overwriting problem within goals reducer

--- client/src/reducers/goalsReducer.js
+++ client/src/reducers/goalsReducer.js
@@ -1,9 +1,11 @@
-import { FETCH_MEMORIZED } from '../actions/types';
+import { FETCH_MEMORIZED, FETCH_BIBLE_READING } from '../actions/types';
 
-export default function(state = {}, action) {
+export default function(state = [], action) {
   switch (action.type) {
     case(FETCH_MEMORIZED):
-      return action.payload;
+      return [...state, {bible_memory: action.payload.data}];
+    case(FETCH_BIBLE_READING):
+      return [...state, {bible_reading: action.payload.data}];
     default:
       return state;
   }
