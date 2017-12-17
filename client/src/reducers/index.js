c054ddcd79b851cc0c7c4560a39a3a3a826d7051
currymich <currymich@gmail.com>
2017-12-17 11:25:36 -0800

    attaching form to the reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import goalsReducer from './goalsReducer';
 import pointsReducer from './pointsReducer';
+import { reducer as reduxFormReducer } from 'redux-form';
 
 export default combineReducers({
   auth: authReducer,
   goals: goalsReducer,
-  points: pointsReducer
+  points: pointsReducer,
+  form: reduxFormReducer
 });
