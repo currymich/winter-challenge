9a56713a88c8fcf08bb09279b0f1a586a7f579b6
currymich <currymich@gmail.com>
2017-12-18 15:12:17 -0800

    Properly working post route for goals

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,36 +1,42 @@
-import axios from 'axios';
-import { FETCH_USER, FETCH_MEMORIZED, FETCH_BIBLE_READING, UPDATE_USER_POINTS } from './types';
+import axios from "axios";
+import {
+	FETCH_USER,
+	FETCH_MEMORIZED,
+	FETCH_BIBLE_READING,
+	UPDATE_USER_POINTS
+} from "./types";
 
 export const fetchUser = () => {
-  return async dispatch => {
-    const user = await axios.get('/api/current_user');
-    dispatch({type: FETCH_USER, payload: user});
+	return async dispatch => {
+		const user = await axios.get("/api/current_user");
+		dispatch({ type: FETCH_USER, payload: user });
 
-    const goals = await axios.get(`/goals/user`);
-    dispatch(updateUserPoints(goals))
-  }
+		const goals = await axios.get(`/goals/user`);
+		dispatch(updateUserPoints(goals));
+	};
 };
 
-export const updateUserPoints = (goals) => {
-  return {
-    type: UPDATE_USER_POINTS,
-    payload: goals
-  }
-}
+export const updateUserPoints = goals => {
+	return {
+		type: UPDATE_USER_POINTS,
+		payload: goals
+	};
+};
 
 export const fetchMemorizedVerses = () => {
-  return async dispatch => {
-    const res = await axios.get('/goals/bible_memory');
-    dispatch({type: FETCH_MEMORIZED, payload: res});
-  }
-}
+	return async dispatch => {
+		const res = await axios.get("/goals/bible_memory");
+		dispatch({ type: FETCH_MEMORIZED, payload: res });
+	};
+};
 
 export const fetchBibleReading = () => {
-  return async dispatch => {
-    const res = await axios.get('/goals/bible_reading');
-    dispatch({type: FETCH_BIBLE_READING, payload: res});
-  }
-}
+	return async dispatch => {
+		const res = await axios.get("/goals/bible_reading");
+		dispatch({ type: FETCH_BIBLE_READING, payload: res });
+	};
+};
+
 export function createGoal(values, type) {
 	const endpointUrl = type => {
 		switch (type) {
