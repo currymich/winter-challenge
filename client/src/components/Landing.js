96948253666bc14ae0a97c7409ea5b408b24f6cb
michael chen <mi2q25q@l-sff21bg8wm-m.gapinc.gap.com>
2017-12-18 00:18:58 -0800

    css update 12/18 12:18AM

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,15 +5,31 @@ const Landing = () => {
     <div className='wc-landing--wrapper'>
       <div className='wc-landing--heading'>
         <h1>
-          Element Winter Challenge!
+          Element Winter Challenge
         </h1>
       </div>
       <div className='wc-landing--section'>
-        Track progress on your winter challenge goals, earn points, win prizes!
+        <p>Track progress on your winter challenge goals, earn points, win prizes!</p>
       </div>
       <div className='wc-landing--section leaderboard'>
         <h3>Leaderboard</h3>
-        <p>*Under Construction*</p>
+        <ul className='leaderboard-list'>
+          <li className='leaderboard-profile'>
+            <div className='leaderboard-rank'>1</div>
+            <div className='leaderboard-name'>Ming Mang</div>
+            <div className='leaderboard-points'>300</div>
+          </li>
+          <li className='leaderboard-profile'>
+            <div className='leaderboard-rank'>2</div>
+            <div className='leaderboard-name'>Ching Chang</div>
+            <div className='leaderboard-points'>200</div>
+          </li>
+          <li className='leaderboard-profile'>
+            <div className='leaderboard-rank'>3</div>
+            <div className='leaderboard-name'>Ding Dang</div>
+            <div className='leaderboard-points'>100</div>
+          </li>
+        </ul>
       </div>
     </div>
   )
