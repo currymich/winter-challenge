c6043c41f9c24246d2052dc7067c03b70e0bfb3b
currymich <currymich@gmail.com>
2017-12-20 11:12:04 -0800

    Updating verse list

--- client/src/static_data/verseList.js
+++ client/src/static_data/verseList.js
@@ -3,7 +3,7 @@ const verseList = [
   { id: 2, reference: "Psalm 23:1-6" },
   { id: 3, reference: "Isaiah 40:27-31" },
   { id: 4, reference: "Habakkuk 3:17-18" },
-  { id: 5, reference: "John 15:1-5" },
+  { id: 5, reference: "John 15:5-8" },
   { id: 6, reference: "Romans 8:1-3a" },
   { id: 7, reference: "1 Corinthians 9:24-27" },
   { id: 8, reference: "Philippians 4:4-7" },
