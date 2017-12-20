6215d240f213c72e42e7fc74c358e3bd7dbe0644
Ben Park <bpark@iparadigms.com>
2017-12-20 00:08:51 -0800

    verselist

---
+++ client/src/static_data/verseList.js
@@ -0,0 +1,14 @@
+const verseList = [
+  { id: 1, reference: "Genesis 15:1-6" },
+  { id: 2, reference: "Psalm 23:1-6" },
+  { id: 3, reference: "Isaiah 40:27-31" },
+  { id: 4, reference: "Habakkuk 3:17-19" },
+  { id: 5, reference: "John 15:1-5" },
+  { id: 6, reference: "Romans 8:1-3a" },
+  { id: 7, reference: "1 Corinthians 9:24-27" },
+  { id: 8, reference: "Philippians 4:4-7" },
+  { id: 9, reference: "Hebrews 12:1-3" },
+  { id: 10, reference: "1 John 1:5-9" }
+];
+
+export default verseList;
