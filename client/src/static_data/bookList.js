f59f9dd1c51a3efc4c151d398512365472805b14
currymich <currymich@gmail.com>
2017-12-19 23:28:45 -0800

    Progress on bookList, pulling in properly but render broken

---
+++ client/src/static_data/bookList.js
@@ -0,0 +1,41 @@
+const bookList = [
+	{ bookTitle: "Case for Christmas", author: "Lee Strobel", points: 100 },
+	{ bookTitle: "Book by Chuck Colson", author: "", points: 100 },
+	{ bookTitle: "Fiction Book by CS Lewis", author: "", points: 100 },
+	{
+		bookTitle: "Shepherd Looks at Psalm 23",
+		author: "Phillip Keller",
+		points: 100
+	},
+	{ bookTitle: "Case for Grace", author: "Lee Strobel", points: 150 },
+	{ bookTitle: "Tactics", author: "Greg Koukl", points: 150 },
+	{ bookTitle: "Gospel", author: "JD Greear", points: 150 },
+	{ bookTitle: "Do Hard Things", author: "Alex Harris", points: 150 },
+	{ bookTitle: "Ask Me Anything", author: "J Budziszewski", points: 150 },
+	{ bookTitle: "Dug Down Deep", author: "Joshua Harris", points: 150 },
+	{ bookTitle: "The Hiding Place", author: "Corrie Ten Boom", points: 150 },
+	{
+		bookTitle: "Stop Asking Jesus Into Your Heart",
+		author: "JD Greear",
+		points: 150
+	},
+	{ bookTitle: "The Hobbit", author: "JRR Tolkien", points: 150 },
+	{ bookTitle: "Letters From A Skeptic", author: "Gregory Boyd", points: 150 },
+	{
+		bookTitle: "Shadow of the Almighty",
+		author: "Elisabeth Elliot",
+		points: 200
+	},
+	{ bookTitle: "On Guard", author: "William Lane Craig", points: 200 },
+	{
+		bookTitle: "Satan and his Kingdom",
+		author: "Dennis McCallum",
+		points: 200
+	},
+	{ bookTitle: "Case for Christ", author: "Lee Strobel", points: 200 },
+	{ bookTitle: "The Lord of the Rings", author: "JRR Tolkien", points: 200 },
+	{ bookTitle: "Mere Christianity", author: "CS Lewis", points: 200 },
+	{ bookTitle: "The Calvary Road", author: "Roy Hession", points: 200 }
+];
+
+export default bookList;
