694bd575974c3f21e710c50d471967700964a9b8
Mike Curry <michael.curry@autodesk.com>
2022-01-06 14:44:50 -0800

    base scoreboard width on avg score

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -32,24 +32,24 @@ const Scoreboard = () => {
 
   const renderChart = (data) => {
     const points = data.map((d) => {
-      if (d.uniqUsers) {
-        return Math.round(d.points / d.uniqUsers * 100) / 100;
+      if (d.uniqUsers && d.uniqUsers.length) {
+        return Math.round(d.points / d.uniqUsers.length * 100) / 100;
       }
       return 0;
     });
 
-    var max = data.map(d => d.points).reduce((a, b) => {
+    var max = points.reduce((a, b) => {
       return Math.max(a, b);
     }, 0);
 
     data.forEach((d, i) => {
-      d.width = 100 * (d.points / max);
+      d.width = 100 * (points[i] / max);
     });
 
     return data.map((d, i) => {
       return (
         <div key={d.team} className="bar" style={{ width: d.width + "%" }}>
-          {`${d.team} (total: ${d.points})`}
+          {`${d.team} (avg: ${points[i]}) (total: ${d.points})`}
         </div>
       );
     });
