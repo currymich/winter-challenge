7fc812825795545aa32b1b679a73d88c3873b0b5
Mike Curry <michael.curry@autodesk.com>
2022-01-25 12:43:46 -0800

    allow pass reset

--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,7 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
-const { validPassword } = require("./authHelpers");
+const { validPassword, genPassword } = require("./authHelpers");
 
 // Local Packages
 const keys = require("../config/keys");
@@ -23,12 +23,19 @@ passport.deserializeUser((id, done) => {
 
 passport.use(
   new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+    console.log(email, password);
     User.findOne({ email: email }, (err, user) => {
       if (err) {
         return done(err);
       }
       if (!user) {
         return done(null, false, { message: "Email not found" });
+      }
+      if (user && !user.password && !user.salt) {
+        const { salt, hash } = genPassword(password);
+
+        User.updateOne({ email }, { $set: { password: hash, salt: salt } });
+        return done(null, user);
       }
       if (!validPassword(password, user.password, user.salt)) {
         return done(null, false, { message: "Incorrect password" });
